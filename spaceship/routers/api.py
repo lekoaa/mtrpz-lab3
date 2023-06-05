459ad84ed0c355e8ed060b88939b94be5f285e8a
Andriyaschenko Vladimir <vovan4ik041@gmail.com>
2023-06-05 18:15:34 +0300

    add to matrix to combine them

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,3 +1,4 @@
+import numpy as np
 from fastapi import APIRouter
 
 router = APIRouter()
@@ -6,3 +7,11 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('')
+def matrix() -> dict:
+    matrix_a = np.random.rand(10, 10).tolist()
+    matrix_b = np.random.rand(10, 10).tolist()
+    product = np.dot(matrix_a, matrix_b).tolist()
+    result = {"matrix_a": matrix_a, "matrix_b": matrix_b, "product":product}
+    return result
